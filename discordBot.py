f69665ec3f89010e2545200ac97772f52aae8689
Twan Hoogveld <31695469+thoogveldHU@users.noreply.github.com>
2021-02-26 15:00:15 +0100

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -10,15 +10,17 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author != client.user:
+    if message.author != client.user: #If message is not from the bot itself
+        
         if message.content.startswith("$restart"):
-            if message.author.guild_permissions.administrator: #isAdmin
+            if message.author.guild_permissions.administrator: #User that send the message is admin
+                
                 await message.channel.send("Restarting the Valheim Server now.")
-                os.system('ls')
+                #Run the sh file from here, for now disabled since it's a test.
                 #subprocess.call(['sh','./restartValheimDocker.sh'])
+                
             else:
                 await message.channel.send("You're not an admin.")
-
 TOKEN = None
 with open('token.txt') as f:
     TOKEN = f.readlines()[0]
