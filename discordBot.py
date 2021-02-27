0f144899506ffb5165814c4102173cef72c39812
Twan <twan.hoogveld@student.hu.nl>
2021-02-27 19:40:35 +0100

    update

--- discordBot.py
+++ discordBot.py
@@ -42,20 +42,22 @@ async def on_message(message):
         elif message.content.lower().startswith("$ip"): #Works
             await message.reply(str(socket.gethostbyname('valheim.hoogveld.me')))
 
-        elif message.content.lower().startswith("$save_backup"): #needs works            
-            currentWorldFolder = '../appdata/valheim2/.config'
-            backupFolder = '../appdata/valheim2/Backups/'
-            
-            #Zip
-            zipf = zipfile.ZipFile("python.zip",'w',zipfile.ZIP_DEFLATED)
-            zipdir(currentWorldFolder,zipf)
-            zipf.close()
+        elif message.content.lower().startswith("$save_backup"): #needs works
+            try:         
+                currentWorldFolder = '../appdata/valheim2/.config'
+                backupFolder = '../appdata/valheim2/Backups/'
+                
+                #Zip
+                zipf = zipfile.ZipFile("python.zip",'w',zipfile.ZIP_DEFLATED)
+                zipdir(currentWorldFolder,zipf)
+                zipf.close()
 
-            #Place in backup folder.
-            #TO-DO
-            
-            await message.add_reaction('👍')
-            pass
+                #Place in backup folder. The python.zip is in the root of the docker container.
+                os.rename('python.zip',backupFolder)
+                await message.add_reaction('👍')
+            except:
+                await message.add_reaction('👎')
+
 
         elif message.content.lower().startswith('$download_backup'): #needs work
             await message.add_reaction('👍')
